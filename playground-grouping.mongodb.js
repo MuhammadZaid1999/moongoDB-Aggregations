851ef2687e923296817e72f3ce9f0eaae0cf2636
MuhammadZaid1999 <zaid.aptechiic@gmail.com>
2024-05-29 23:23:26 +0500

    first commit

---
+++ playground-grouping.mongodb.js
@@ -0,0 +1,110 @@
+/* global use, db */
+// MongoDB Playground
+// To disable this template go to Settings | MongoDB | Use Default Template For Playground.
+// Make sure you are connected to enable completions and to be able to run a playground.
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
+// The result of the last command run in a playground is shown on the results panel.
+// By default the first 20 documents will be returned with a cursor.
+// Use 'console.log()' to print to the debug output.
+// For more documentation on playgrounds please refer to
+// https://www.mongodb.com/docs/mongodb-vscode/playgrounds/
+
+// Select the database to use.
+use('aggree');
+
+
+// Here we run an aggregation and open a cursor to the results.
+// Use '.toArray()' to exhaust the cursor to return the whole result set.
+// You can use '.hasNext()/.next()' to iterate through the cursor page by page.
+
+
+
+const groupByGender = db.getCollection('users').aggregate([
+    {
+      $group: {
+        _id: '$gender'
+      }
+    },
+]);
+
+const groupByAge = db.getCollection('users').aggregate([
+    {
+      $group: {
+        _id: '$age'
+      }
+    },
+]);
+
+
+const averageAge = db.getCollection('users').aggregate([
+    {
+      $group: {
+        _id: null,
+        averageAge:{
+        $avg: "$age"
+        }    
+      }
+    },
+]);
+
+const groupByGenderAndAverageAge = db.getCollection('users').aggregate([
+    {
+      $group: {
+        _id: "$gender",
+        averageAge:{
+        $avg: "$age"
+        }    
+      }
+    },
+]);
+
+const groupAndCount = db.getCollection('users').aggregate([
+    {
+      $group: {
+        _id: "$favoriteFruit",
+        quantity:{
+        $sum: 1
+        }    
+      }
+    },
+]);
+
+const groupCountAndSort = db.getCollection('users').aggregate([
+    {
+      $group: {
+        _id: "$favoriteFruit",
+        quantity:{
+        $sum: 1
+        }    
+      }
+    },
+    {
+        $sort: {
+          quantity: -1
+        }
+    }
+]);
+
+
+const groupCountAndSortAndimit = db.getCollection('users').aggregate([
+    {
+      $group: {
+        _id: "$favoriteFruit",
+        quantity:{
+        $sum: 1
+        }    
+      }
+    },
+    {
+        $sort: {
+          quantity: -1
+        }
+    },
+    {
+        $limit: 2
+    }
+]);
+
+
+
+
